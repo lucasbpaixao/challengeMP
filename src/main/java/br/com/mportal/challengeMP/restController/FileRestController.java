473f8ebb2321990bb80c3c40f228474983361c46
lucasbpaixao <lucasbpaixao@outlook.com>
2022-01-31 02:47:14 -0300

    Tela realizando upload e download

--- src/main/java/br/com/mportal/challengeMP/restController/FileRestController.java
+++ src/main/java/br/com/mportal/challengeMP/restController/FileRestController.java
@@ -5,20 +5,25 @@ import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.unbescape.csv.CsvEscape;
 
 import br.com.mportal.challengeMP.dto.MetricsDto;
 import br.com.mportal.challengeMP.model.Person;
+import br.com.mportal.challengeMP.repository.PersonRepository;
 import br.com.mportal.challengeMP.service.PersonService;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -30,12 +35,15 @@ public class FileRestController {
 
     @Autowired
     private PersonService personService;
+
+    @Autowired
+    private PersonRepository personRepository;
     
     @PostMapping("/upload")
     @Transactional
     public ResponseEntity<MetricsDto> uploadFile(@RequestBody MultipartFile csvFile ) throws IOException, ParseException {
 
-        //TODO: Terminar adaptação para funcionar como uma API, já foi feita a correção das datas, aprimorar tambem o algoritmo das datas para caso a pessoa tenha nascido depois da data atual
+        //TODO: mudar lugar de persistencias dos dados e fazer o botão de download do csv
 
         CSVParser csvParser = CSVFormat.EXCEL.withHeader().parse(new InputStreamReader(csvFile.getInputStream()));
 
@@ -45,7 +53,25 @@ public class FileRestController {
 
         MetricsDto metricsDto = personService.calculateMetrics(persons);
 
+        personRepository.deleteAll();
+
+        for (Person person : persons) {
+            personRepository.save(person);
+        }
+
+        personRepository.flush();
+
         return ResponseEntity.ok().body(metricsDto);
     }
-    
+
+    @GetMapping("/download/csv_teste_download.csv")
+    @Transactional
+    public void downloadFile(HttpServletResponse response) throws IOException {
+
+        List<Person> persons = personRepository.findAll();
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; file=csv_teste_download.csv");
+        
+        personService.writeCsv(response.getWriter(), persons);
+    }
 }
